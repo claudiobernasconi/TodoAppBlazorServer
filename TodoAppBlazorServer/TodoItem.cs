2a3201995f6bbf2fef6bcfe9d60cd65d75c49201
Claudio Bernasconi <email@claudio-bernasconi.net>
2023-05-12 15:07:04 +0200

    Listing todo items.

---
+++ TodoAppBlazorServer/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAppBlazorServer;
+
+public class TodoItem
+{
+    public TodoItem(string text)
+    {
+        Text = text;
+    }
+
+    public string Text { get; set; }
+    public bool Completed { get; set; }
+}
